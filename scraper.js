baee539a7e997b8be462b01416bef539dfde9ae7
ors <prestalabx@gmail.com>
2017-07-26 16:02:30 +0300

    first commit

---
+++ scraper.js
@@ -0,0 +1,154 @@
+var log     = require('cllc')();
+var tress   = require('tress');
+var needle  = require('needle');
+var cheerio = require('cheerio');
+var fs      = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+
+
+var sCookie = 'http://www.ssa-archery.be/products.php?lang=en';
+
+var httpOptions = {};
+var results     = [];
+var urls     = [];
+
+var q = tress(crawl, 1);
+
+// q.success = function () {
+// 	q.concurrency = 1;
+// };
+
+
+
+// q.retry = function () {
+// 	q.concurrency = -10000;
+// }
+// q.error = function () {
+// 	log('q.failed', q.failed);
+// }
+
+q.drain = function () {
+	fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
+	var db = new sqlite3.Database('data.sqlite');
+	db.on("error", function(error) {
+	    console.log(error);
+	}); 
+
+	console.log('failed', q.failed);
+	// console.log(results);
+	db.serialize(function () {
+		// db.run('DROP TABLE IF EXISTS data');
+		db.run('CREATE TABLE IF NOT EXISTS data (url TEXT, group_name TEXT, class_name TEXT, brand TEXT, reference PRIMARY KEY, price REAL, avb NUMERIC, img TEXT, name TEXT)');
+		var stmt = db.prepare('INSERT OR REPLACE INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+
+		Object.keys(results[0]).map(function (key) {
+			r = results[0][key];
+			//stmt.run(results[0][key]);
+			stmt.run(Object.keys(r).map(function (key) {return r[key];})); 
+			
+		});
+		stmt.finalize();
+		db.close();
+	});
+	log(q.failed);
+	log.finish();
+	log('Работа закончена');
+	
+};
+
+var options = {
+	follow_max: 1,
+	follow_set_cookies:true,
+	follow_set_referer:true,
+	user_agent: "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"
+};
+
+needle.get(sCookie, options, function (err, res) {
+	if (err || res.statusCode !== 200)
+		throw err || res.statusCode;
+	
+	httpOptions.cookies    = res.cookies;
+	httpOptions.user_agent = "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
+	log('Начало работы');
+	console.log(res.cookies);
+	q.push({url: 'http://www.ssa-archery.be/products.php?sort=group', type: 'main'});
+	//q.push({url: 'http://www.ssa-archery.be/products.php?cat=STABILIZERS+%26+DAMPERS+%26+PARTS&class=V-BAR+EXTENDERS&sort=group', type: 'class', group_name: 'x', class_name: 'y'});
+});
+
+
+function crawl(data, callback) {
+	needle.get(data.url, httpOptions, function (err, res) {
+		// console.log('needle');
+		var statusCode = 200;
+		if (err || res.statusCode !== 200) {
+			q.concurrency === 1 && log.e((err || res.statusCode) + ' - ' + url);
+			
+			
+			statusCode = res.statusCode;
+			// return callback(new Error('302'));
+			// return callback(null);
+		}
+
+		var $ = cheerio.load(res.body);
+
+		if (data.type == 'class') {
+			product = {};
+			reference_old = '';
+			$('tr',$('table').first()).each(function(i, tr){
+				row = $('td', this);
+				reference = row.eq(2).text();
+				name = row.eq(1).text().trim();
+				if ((reference != '')&&(reference != ' ')&&(reference != 'Code/Description')&&(reference != ' €')){
+
+					product[reference] = {};
+					product[reference]['url'] = data.url;
+					product[reference]['group_name'] = data.group_name;
+					product[reference]['class_name'] = data.class_name;
+					product[reference]['brand'] = name;
+					product[reference]['reference'] = reference;
+					product[reference]['price'] = row.eq(4).text().trim();
+					avb = $('td', row.eq(5)).prop('style');
+					if (avb['background-color'] == 'green') {
+						product[reference]['avb'] = 1;
+					} else if (avb['background-color'] == 'orange') {
+						product[reference]['avb'] = 2;
+					} else {
+						product[reference]['avb'] = 0;
+					}
+					img = $('a', row.eq(0)).prop('href');
+					if (img != 'look/blank.gif') {
+						product[reference]['img'] = 'http://www.ssa-archery.be/'+img;
+					} else {
+						product[reference]['img'] = '';
+					}
+					reference_old = reference;
+				} else if ((name != '')&&(name != 'Brand')) {
+					product[reference_old]['name'] = name;
+				}
+			});
+
+			results = results.concat(product);
+
+			urls.push(data.url);
+			$('.button').each(function(i, a){
+				new_url = 'http://www.ssa-archery.be/products.php'+$(this).prop('href');
+
+				if ((new_url.indexOf('page=1') == -1)&&(urls.indexOf(new_url) == -1)) {
+					urls.push(new_url);
+					q.push({url: new_url, type: 'class', group_name: data.group_name, class_name: data.class_name});
+				}
+			})
+		} else if (data.type == 'main') {
+			$('.list-group-item').each(function(i, a){
+				q.push({url: 'http://www.ssa-archery.be/'+$(this).prop('href'), type: 'group', group_name: $('td', this).text().trim()});
+			})
+		} else if (data.type == 'group') {
+			$('.list-group-item').each(function(i, a){
+				q.push({url: 'http://www.ssa-archery.be/'+$(this).prop('href'), type: 'class', group_name: data.group_name, class_name: $('td', this).text().trim()});
+			})
+		}
+		console.log(data.url);
+		log.step(0, 0, 1);
+		callback();
+	});
+}
